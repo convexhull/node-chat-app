7506e5ea0378071c185d7dbe72873bebd1d5d2b6
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-03-26 16:26:20 +0530

    minor changes for heroku deployment

--- server/server.js
+++ server/server.js
@@ -2,12 +2,11 @@ const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-const ip = '127.0.0.1';
 const app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port, ip , () => {
-    console.log(`Server is up and running on Port ${port} and IP ${ip}`);
+app.listen(port, () => {
+    console.log(`Server is up and running on Port ${port}`);
 })
 
